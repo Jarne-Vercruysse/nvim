375410aa2d4a09e45a7a3020cc735d84363e3116
Jarne-Vercruysse <jarnevercruysse@proton.me>
2025-08-19 01:15:31 +0200

    feat: installed treesitter

---
+++ lua/jarver/configs/treesitter.lua
@@ -0,0 +1,29 @@
+local M = {}
+local parsers = { "rust", "lua", "just" }
+
+M.setup = function()
+	local ts = require("nvim-treesitter")
+	ts.install(parsers)
+
+	-- enable TS features per filetype
+	local aug = vim.api.nvim_create_augroup("JarverTS", { clear = true })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = aug,
+		pattern = parsers,
+		callback = function()
+			-- start treesitter highlight
+			vim.treesitter.start()
+
+			-- folds with TS
+			vim.wo.foldmethod = "expr"
+			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+			-- indent with TS
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end,
+	})
+end
+
+M.setup()
+
+return M
