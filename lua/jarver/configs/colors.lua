10fe92f5d380cfd278a6db0a5d651b3ab14924b2
Jarne-Vercruysse <jarnevercruysse@proton.me>
2025-08-19 02:25:10 +0200

    feat: toggle colorschemes

---
+++ lua/jarver/configs/colors.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+M.themes = {
+	gruvbox = "gruvbox",
+	kanagawa = "kanagawa",
+}
+
+M.current = M.themes.gruvbox
+
+-- Set a new theme
+function M.set(theme)
+	vim.cmd.colorscheme(theme)
+	M.current = theme
+	vim.schedule(function()
+		vim.notify("Colorscheme set to " .. theme, vim.log.levels.INFO)
+	 end)
+end
+
+-- toggle tussen kanagawa en gruvbox
+function M.toggle()
+  if M.current == M.themes.kanagawa then
+    M.set(M.themes.gruvbox)
+  else
+    M.set(M.themes.kanagawa)
+  end
+end
+
+-- keybind om te togglen (bv. <leader>tc)
+vim.keymap.set("n", "<leader>tcs", function() M.toggle() end, { desc = "Toggle colorscheme" })
+
+-- initial load
+M.set(M.current)
+
+return M
