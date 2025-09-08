b87900491abb8fae144b6c09dab25c021242363c
Jarne-Vercruysse <jarnevercruysse@proton.me>
2025-09-08 15:36:51 +0200

    feat: enable signatures

--- lua/jarver/plugins/blink.lua
+++ lua/jarver/plugins/blink.lua
@@ -8,7 +8,8 @@ return {
       appearance = {
         nerd_font_variant = "mono",
       },
-      completion = { documentation = { auto_show = false } },
+      completion = { documentation = { auto_show = true } },
+      signature = { enabled = true },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
